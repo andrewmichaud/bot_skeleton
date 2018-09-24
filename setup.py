3fe12929ebe42412dafdcb50ba4d018257001ca7
Andrew Michaud <dev@mail.andrewmichaud.com>
2018-09-23 23:04:28 -0700

    declare production ready, some opinionated cleanup

--- setup.py
+++ setup.py
@@ -4,18 +4,21 @@ from setuptools import setup, find_packages
 
 HERE = path.abspath(path.dirname(__file__))
 
-INSTALL_REQUIRES = ["clint>=0.5.1, <0.6.0",
-                    "drewtilities>=1.0.5, <2.0.0",
-                    "tweepy>=3.6, <4.0",
-                    "Mastodon.py>=1.3.1, <2.0",
-                    ]
+INSTALL_REQUIRES = [
+    "clint>=0.5.1, <0.6.0",
+    "drewtilities>=1.0.5, <2.0.0",
+    "tweepy>=3.6, <4.0",
+    "Mastodon.py>=1.3.1, <2.0",
+]
 
-TESTS_REQUIRE = ["coveralls>=1.5.0, <2.0.0",
-                 "pytest>=3.8.0, <4.0.0"
-                 ]
+TESTS_REQUIRE = [
+    "coveralls>=1.5.0, <2.0.0",
+    "pytest>=3.8.0, <4.0.0",
+]
 
-SETUP_REQUIRES = ["pytest-runner"
-                  ]
+SETUP_REQUIRES = [
+    "pytest-runner",
+]
 
 with open(path.join(HERE, "README.rst")) as f:
     LONG_DESCRIPTION = f.read().strip()
@@ -26,19 +29,20 @@ with open(path.join(HERE, "VERSION"), encoding="utf-8") as f:
 setup(author="Andrew Michaud",
       author_email="bots@mail.andrewmichaud.com",
 
-      classifiers=["Development Status :: 3 - Alpha",
-                   "Environment :: Console",
-                   "Intended Audience :: Developers",
-                   "License :: OSI Approved :: BSD License",
-                   "Natural Language :: English",
-                   "Operating System :: MacOS :: MacOS X",
-                   "Operating System :: POSIX :: Linux",
-                   "Operating System :: POSIX :: BSD :: FreeBSD",
-                   "Programming Language :: Python :: 3.6",
-                   "Programming Language :: Python :: 3 :: Only",
-                   "Programming Language :: Python :: Implementation :: CPython",
-                   "Topic :: Software Development :: Libraries"
-                  ],
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Natural Language :: English",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: POSIX :: Linux",
+          "Operating System :: POSIX :: BSD :: FreeBSD",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3 :: Only",
+          "Programming Language :: Python :: Implementation :: CPython",
+          "Topic :: Software Development :: Libraries",
+      ],
 
       entry_points={},
 
